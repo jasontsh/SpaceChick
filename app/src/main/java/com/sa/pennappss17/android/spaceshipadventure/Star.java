82cc3cf6d1b2ae15075233d5d66fbf203d482d69
Gustavo Aviles <gustavo@huntsman-ve565-1109.apn.wlan.upenn.edu>
2017-01-21 18:26:00 -0500

    stars

--- app/src/main/java/com/sa/pennappss17/android/spaceshipadventure/Star.java
+++ app/src/main/java/com/sa/pennappss17/android/spaceshipadventure/Star.java
@@ -1,17 +1,44 @@
 package com.sa.pennappss17.android.spaceshipadventure;
 
+import android.graphics.Bitmap;
+
 /**
  * Created by gustavo on 1/21/17.
  */
 
-public class Star {
+public class Star implements GameObj{
     private int xStarPosition;
     private int yStarPosition;
     private int velocity;
 
     public Star(int width, int length) {
-        xStarPosition = (int) Math.random() * width;
-        yStarPosition = (int) Math.random() * length;
+        xStarPosition = (int) (Math.random() * width);
+        yStarPosition = (int) (Math.random() * length);
         velocity = 5;
+    }
+
+    @Override
+    public Bitmap getBitmap() {
+        return null;
+    }
+
+    @Override
+    public int getX() {
+        return xStarPosition;
+    }
+
+    @Override
+    public int getY() {
+        return yStarPosition;
+    }
+
+    @Override
+    public Hitbox getHitbox() {
+        return null;
+    }
+
+    @Override
+    public void movement() {
+        xStarPosition += velocity;
     }
 }
