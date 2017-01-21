4de7823d07449ec37bc5f99e0f3dd93ee95dd48f
jasontsh <tangsh@sas.upenn.edu>
2017-01-20 20:43:56 -0500

    add GameObj and init GameView

---
+++ app/src/main/java/com/sa/pennappss17/android/spaceshipadventure/GameObj.java
@@ -0,0 +1,13 @@
+package com.sa.pennappss17.android.spaceshipadventure;
+
+import android.graphics.Bitmap;
+
+/**
+ * Created by Jason on 1/20/2017.
+ */
+
+public interface GameObj {
+    Bitmap getBitmap();
+    int getX();
+    int getY();
+}
