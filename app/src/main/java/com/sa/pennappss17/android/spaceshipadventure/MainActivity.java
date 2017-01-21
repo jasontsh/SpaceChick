5e36bf917b8456aa08a74691c5ae2476b7f768d8
jasontsh <tangsh@sas.upenn.edu>
2017-01-20 20:07:41 -0500

    Initial commit

---
+++ app/src/main/java/com/sa/pennappss17/android/spaceshipadventure/MainActivity.java
@@ -0,0 +1,13 @@
+package com.sa.pennappss17.android.spaceshipadventure;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
