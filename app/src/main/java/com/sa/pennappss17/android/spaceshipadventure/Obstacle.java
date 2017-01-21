5fdaa9838f30221e90ba9883cbce783d796656de
Emily Tan <emilytan@huntsman-ve565-2173.apn.wlan.upenn.edu>
2017-01-20 23:00:43 -0500

    add some stuff in obstacles

--- app/src/main/java/com/sa/pennappss17/android/spaceshipadventure/Obstacle.java
+++ app/src/main/java/com/sa/pennappss17/android/spaceshipadventure/Obstacle.java
@@ -8,6 +8,18 @@ import android.graphics.Bitmap;
 
 public class Obstacle implements GameObj {
     private double xPosition;
+    private final double yPosition;
+    private double velocity;
+
+    public Obstacle(int xInitial, int boundary) {
+        xPosition = xInitial;
+        yPosition = Math.random() * boundary;
+        velocity = Math.random() * 5 + 5;
+    }
+
+    public void movement() {
+        xPosition = -1 * velocity;
+    }
 
     @Override
     public Bitmap getBitmap() {
